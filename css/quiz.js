f5bd1f6c35e9eaea55c41871c9e6b372df826e2e
손민주 <rhktlr19@naver.com>
2022-07-13 12:33:51 +0900

    d

--- css/quiz.js
+++ css/quiz.js
@@ -1,6 +1,2 @@
-let vh = window.innerHeight * 0.01
-document.documentElement.style.setProperty('--vh', `${vh}px`)
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-})
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
